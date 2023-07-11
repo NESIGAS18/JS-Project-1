3647231dc9a6ebc9db54a0c7fb72c4988deef065
Nesiga S <129278009+NESIGAS18@users.noreply.github.com>
2023-07-11 16:23:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var a = 6;
+var b = 3;
+var c = a + b;
+var d = a - b;
+var e = a * b;
+var f = a / b;
+
+function add(){
+    document.getElementById("add").innerHTML=c;
+}
+
+function sub(){
+    document.getElementById("sub").innerHTML=d;
+}
+
+function mul(){
+    document.getElementById("mul").innerHTML=e;
+}
+
+function div(){
+    document.getElementById("div").innerHTML=f;
+}
